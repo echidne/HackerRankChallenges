07ccf2065fdb2960a9a277ef79fab569d16039ee
echidne <65033881+echidne@users.noreply.github.com>
2022-06-21 16:58:19 +0200

    Delete challenge HackerRanck Validating UID.py

--- challenge HackerRanck Validating UID.py
+++
@@ -1,9 +0,0 @@
-import re
-def valid_uid(string):
-    if 2<len(re.findall(r"[A-Z]", string)) and len(re.findall(r"\d", string))>3 and re.match(r"[a-zA-Z0-9]{10}", string) and len(set(string)) ==10 :
-        return "Valid"
-    else:
-        return "Invalid"
-
-for _ in range(int(input())):
-    print(valid_uid(input()))
